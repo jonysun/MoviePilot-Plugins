96a6a336581e33e80c4b3d7aa435d322638f73cf
Jonysun <followsuns@gmail.com>
2025-08-16 14:33:08 +0800

    回退

--- plugins.v2/brushflowplus/__init__.py
+++ plugins.v2/brushflowplus/__init__.py
@@ -255,7 +255,7 @@ class BrushFlowPlus(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "1.0.9"
+    plugin_version = "1.1.2"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer,jonysun"
     # 作者主页
